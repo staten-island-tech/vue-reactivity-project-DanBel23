03b7bc5a6af53a54c90dfb5d3ba6b39c1436e68b
DanBel23 <daniel23.bel@gmail.com>
2021-10-22 12:27:44 -0400

    Update App.vue

--- vue-reactivity-project-danbel23/src/App.vue
+++ vue-reactivity-project-danbel23/src/App.vue
@@ -3,12 +3,14 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1>{{ title }}</h1>
-    <button>{{ chess1 }}</button>
-    <button>{{ chess2 }}</button>
-    <button>{{ chess3 }}</button>
-    <button>{{ chess4 }}</button>
-    <button>{{ chess5 }}</button>
-    <button>{{ chess6 }}</button>
+    <div class="chessbuttons">
+      <button>{{ chess1 }}</button>
+      <button>{{ chess2 }}</button>
+      <button>{{ chess3 }}</button>
+      <button>{{ chess4 }}</button>
+      <button>{{ chess5 }}</button>
+      <button>{{ chess6 }}</button>
+    </div>
   </div>
 </template>
 
@@ -45,9 +47,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.button {
+.chessbuttons {
   display: flex;
-  justify-content: center;
-  top: 10rem;
+  justify-content: space-around;
+  align-items: center;
+  margin:3 auto;
+  
 }
 </style>
