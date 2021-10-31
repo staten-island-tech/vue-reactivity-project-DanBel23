ba3b96b26dbec74738effa713f4259987d00a9a3
DanBel23 <71675993+DanBel23@users.noreply.github.com>
2021-10-30 23:27:00 -0400

    10/30/21

--- vue-reactivity-project-danbel23/src/App.vue
+++ vue-reactivity-project-danbel23/src/App.vue
@@ -5,6 +5,10 @@
     <h1>{{ title }}</h1>
     <div class ="cart">
        <button class="cartButton" @click="addtoCart()">Add to Cart!</button>
+       <button class="cartButton resetCart" @click="resetCart()">Reset Cart</button>
+     </div>
+     <div class ="cartAmount">
+       <p>You have {{cartAmount}} points in the cart!</p>
      </div>
     <div class="chessbuttons" >
       <button class ="btn"  v-for = "chessPiece in chessPieces" :key="chessPiece" @click="displayImage(chessPiece)" >{{chessPiece}}</button>
@@ -16,12 +20,10 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data() {
     return {
@@ -33,23 +35,43 @@ export default {
         Knight: require('./assets/Chessknight.jpeg'),
         Bishop: require('./assets/ChessBishop.jpeg'),
         Rook: require('./assets/ChessRook.jpeg'), 
-        Queen: require('./assets/ChessQueen.jpeg'), 
-        // King: require('./assests/ChessKing.jpeg')  
+        Queen: require('./assets/ChessQueen.jpeg'),
+        King: require('./assets/ChessKing.jpeg')  
       },
-      cart: [],
+      cartAmount: 0,
+      clickedPiece: '',
       
     }
   },
   methods: {
     displayImage(piece) { 
       this.currentImage = this.chessImages[piece];
-          
+      this.clickedPiece = piece;    
     },
     addtoCart() {
-      // const cart = document.querySelector("cart");
-      let cart = Number(this.cart);
-      this.cart = cart + 1
-      console.log(this.cart);
+      switch (this.clickedPiece) {
+        case "Pawn":
+          this.cartAmount++;
+          break;
+          case "Knight":
+          this.cartAmount += 3;
+          break;
+          case "Bishop":
+          this.cartAmount += 3;
+          break;
+          case "Rook":
+          this.cartAmount += 5;
+          break;
+          case "Queen":
+          this.cartAmount += 9;
+          break;
+          case "King":
+          this.cartAmount += 22;
+          break;
+      }
+    },
+    resetCart() {
+      this.cartAmount = 0;
     }
   },
 }
@@ -92,9 +114,12 @@ export default {
 .cart {
   display: flex;
   justify-content: center;
+  margin-bottom: 3.5rem;
 }
+
 .cartButton {
   padding: 1rem;
+  margin-right: 3rem;
   display: flex;
   background-color: blue;
   color: gold;
@@ -102,7 +127,10 @@ export default {
   text-align: center;
   border-radius: 1.5rem;
 }
-.cart:hover {
+.resetCart {
+  background-color: purple;
+}
+.cartButton:hover {
   transform: scaleX(1.2) scaleY(1.2);
   border-radius: 0.5rem;
 }
