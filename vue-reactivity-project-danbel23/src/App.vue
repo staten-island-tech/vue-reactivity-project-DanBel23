50146fb7a46e4b8ae76033eb4dd0250561af45e9
DanBel23 <daniel23.bel@gmail.com>
2021-11-01 12:27:21 -0400

    Update App.vue

--- vue-reactivity-project-danbel23/src/App.vue
+++ vue-reactivity-project-danbel23/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1>{{ title }}</h1>
     <div class ="cart">
        <button class="cartButton" @click="addtoCart()">Add to Cart!</button>
